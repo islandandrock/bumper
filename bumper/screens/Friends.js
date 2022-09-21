c532c43b33345bc19a19528d9f9b922ca6943762
aaaaahhh <blewconklin0264@student.pps.net>
2022-09-21 09:40:55 -0700

    added basic pages

---
+++ bumper/screens/Friends.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+
+
+export default function FriendScreen () {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>friend screen</Text>
+        </View>
+    )
+}
