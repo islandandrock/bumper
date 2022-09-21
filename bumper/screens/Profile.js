53a7349ff463a8500e62bb1e317e4d99a3d70d4e
aaaaahhh <blewconklin0264@student.pps.net>
2022-09-21 10:01:49 -0700

    working on profile screen

---
+++ bumper/screens/Profile.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+
+
+export default function ProfileScreen () {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>profile</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
