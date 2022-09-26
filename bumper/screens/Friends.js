89d7c16a0a9c0a462d2162ccfc37506a0579a3df
aaaaahhh <blewconklin0264@student.pps.net>
2022-09-26 09:54:50 -0700

    added search bar

--- bumper/screens/Friends.js
+++ bumper/screens/Friends.js
@@ -1,10 +1,22 @@
-import {View, Text} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
+import {useState} from 'react'
+
+const SearchBar = (props) => {
+    return (
+        <View>
+            <TextInput styles={{backgroundColor: 'red'}} placeholder='Search' value={props.SearchText} onChangeText={(text)=>props.SetSearchText(text)}/>
+        </View>
+
+    )
+
+}
 
 
 export default function FriendScreen () {
+    const [SearchText, SetSearchText] = useState('');
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>friend screen</Text>
+        <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+            <SearchBar SearchText={SearchText} SetSearchText={SetSearchText}/>
         </View>
     )
 }
