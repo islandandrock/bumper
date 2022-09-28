b47bfc338893804495a4737fd87c8baa1b636c41
aaaaahhh <blewconklin0264@student.pps.net>
2022-09-28 09:57:40 -0700

    toggle button for friend screen added

--- bumper/screens/Friends.js
+++ bumper/screens/Friends.js
@@ -1,10 +1,10 @@
-import {View, Text, TextInput, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
 import {useState} from 'react'
 
 const SearchBar = (props) => {
     return (
         <View>
-            <TextInput styles={{backgroundColor: 'red'}} placeholder='Search' value={props.SearchText} onChangeText={(text)=>props.SetSearchText(text)}/>
+            <TextInput style={styles.input} placeholder='Search' value={props.SearchText} onChangeText={(text)=>props.SetSearchText(text)}/>
         </View>
 
     )
@@ -14,13 +14,48 @@ const SearchBar = (props) => {
 
 export default function FriendScreen () {
     const [SearchText, SetSearchText] = useState('');
+    const [ListMode, SetListMode] = useState(true)
+
     return (
-        <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
-            <SearchBar SearchText={SearchText} SetSearchText={SetSearchText}/>
+        <View>
+            <View style={styles.container}>
+                <SearchBar SearchText={SearchText} SetSearchText={SetSearchText}/>
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: 'flex-end', margin: 10}}>
+                <TouchableOpacity style={styles.toggle} onPress={() => SetListMode(!ListMode)}>
+                    <Text style={{fontSize: 18, fontWeight: 'bold'}}>Toggle Mode</Text>
+                </TouchableOpacity>
+            </View>
+            {ListMode ? 
+            (
+                <View>
+                    <Text>list</Text>
+                </View>
+            ):(
+                <View>
+                    <Text>map</Text>
+                </View>
+            )}
         </View>
     )
 }
 
-const styles = StyleSheet.create(
-    
-)
+const styles = StyleSheet.create({
+    container: {
+        margin: 10
+    },
+
+    toggle: {
+        backgroundColor:'pink',
+        padding: 10,
+        borderRadius: 10
+    },
+
+    input: {
+        borderRadius:10,
+        padding:10,
+        backgroundColor: '#fff',
+        borderWidth:1,
+        borderColor: 'pink'
+    }
+})
