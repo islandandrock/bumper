66a36fe6236ded3b15ab8b4821f9e9e65ab8296b
islandandrock <islandandrock@gmail.com>
2022-09-28 09:02:18 -0700

    Add icons for bottom tab navigator

--- bumper/App.js
+++ bumper/App.js
@@ -2,7 +2,7 @@ import FriendScreen from './screens/Friends';
 import PlateLookupScreen from './screens/PlateLookup';
 import Profile from './screens/Profile';
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -20,9 +20,57 @@ const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Plate Lookup" component={PlateLookupScreen} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Friends" component={FriendScreen} />
+      <Tab.Screen name="Plate Lookup" component={PlateLookupScreen} options={{
+        title: 'Plate Lookup',
+        headerShown: true,
+        tabBarLabel:() => {return null},
+        tabBarIcon: ({ focused }) => {
+          const image = focused
+          ? require('./assets/plate_filled.png')
+          : require('./assets/plate.png')
+          
+          return (
+              <Image
+                  source={image}
+                  style={{height:38, width:38}}
+              />
+          )
+        }
+      }}/>
+      <Tab.Screen name="Profile" component={Profile} options={{
+        title: 'Profile',
+        headerShown: true,
+        tabBarLabel:() => {return null},
+        tabBarIcon: ({ focused }) => {
+          const image = focused
+          ? require('./assets/user_filled.png')
+          : require('./assets/user.png')
+          
+          return (
+              <Image
+                  source={image}
+                  style={{height:38, width:38}}
+              />
+          )
+        }
+      }}/>
+      <Tab.Screen name="Friends" component={FriendScreen} options={{
+        title: 'Friends',
+        headerShown: true,
+        tabBarLabel:() => {return null},
+        tabBarIcon: ({ focused }) => {
+          const image = focused
+          ? require('./assets/people_filled.png')
+          : require('./assets/people.png')
+          
+          return (
+              <Image
+                  source={image}
+                  style={{height:38, width:38}}
+              />
+          )
+        }
+      }}/>
     </Tab.Navigator>
   );
 }
