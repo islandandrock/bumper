3d34ae69b35340f88c5452df9b9807ea20a1784b
islandandrock <islandandrock@gmail.com>
2022-09-30 09:17:38 -0700

    Add basic signup request to frontend

--- bumper/util/requests.js
+++ bumper/util/requests.js
@@ -4,6 +4,39 @@ if (server == "REPLACEME") {
   console.log("Change the IP in server.json!!!")
 }
 
-export function getFromWeb() {
-  let x = fetch(server)
+export class ServerError extends Error {
+  constructor(code, name, description) {
+    super(code);
+    this.name = name;
+    this.code = code;
+    this.description = description;
+  }
+}
+
+const handledFetch = async (resource, init={}) => {
+  init.headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+  const response = await fetch(resource, init)
+  if (!response.ok) {
+    let error = await response.json();
+    throw new ServerError(response.status, error.name, error.description);
+  }
+  return response;
+}
+
+export function isCode(e, codes) {
+  return (e instanceof ServerError && codes.includes(e.code))
+}
+
+export const signUp = async (username, email, password) => {
+  await handledFetch(server + '/signup', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password: password
+    })
+  });
 }
