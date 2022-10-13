a32142bd25d7164699b0e64cd2f85aaf554a3d9f
islandandrock <islandandrock@gmail.com>
2022-10-13 10:01:08 -0700

    Add dummy modal for new app linking

--- bumper/screens/Profile.js
+++ bumper/screens/Profile.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity, Linking, Alert } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Linking, Alert, Modal } from 'react-native';
 import { useState, useEffect } from 'react';
 import { getData } from '../util/storage';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -7,7 +7,7 @@ import { addConnection } from '../util/requests';
 
 const SocialMedia = (props) => {
     return (
-        <TouchableOpacity style={{width: '100%', justifyContent: 'flex-start', flexDirection: 'row', padding: 10}} onPress={async () => {
+        <TouchableOpacity style={{width: '100%', justifyContent: 'flex-start', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, paddingVertical: 10}} onPress={async () => {
             try {
                 await addConnection("Instagram", "instagram.com");
                 Alert.alert("App connected!");
@@ -27,9 +27,48 @@ const SocialMedia = (props) => {
     )
 }
 
+const NewAppModal = (props) => {
+
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={props.modalVisible}
+            onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisible(!props.modalVisible);
+            }}
+            style={{alignContent:"center", alignItems:"center", justifyContent:"center"}}
+        >
+            <View style={{justifyContent:"center", alignItems:"center", height:"100%", width:"100%"}}>
+            <View style={{width:"80%", height:"70%", backgroundColor:"lightpink", borderRadius:10}}>
+                <Text style={{textAlign:"center", fontWeight:"bold", fontSize:20, marginVertical:10}}>Link a New App</Text>
+                <TouchableOpacity
+                    style={{}}
+                    onPress={() => props.setModalVisible(!props.modalVisible)}>
+                    <Text>Hide Modal</Text>
+                </TouchableOpacity>
+                <View style={{width:"100%", flexGrow:1, flexShrink:1, backgroundColor:"mistyrose"}}>
+                    <ScrollView style={{width:"100%"}}>
+                    <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+                    <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+                    <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+                    <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+                    <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+                    <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+                    </ScrollView>
+                
+                </View>
+            </View>
+            </View>
+        </Modal>
+    )
+}
+
 
 export default function ProfileScreen () {
     const [username, setUsername] = useState("")
+    const [modalVisible, setModalVisible] = useState(false);
 
     useEffect(() => {
         const asyncFunc = async () => {
@@ -39,7 +78,10 @@ export default function ProfileScreen () {
     }, [])
     
     return (
-        <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            
+            <NewAppModal modalVisible={modalVisible} setModalVisible={setModalVisible}/>
+
             <View style={{justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'column', width: '100%', height: 220, backgroundColor: 'pink'}}>
                 <Text style={{fontSize: 40, fontWeight: 'bold', padding: 40}}>License Plate</Text>
                 <View style={{flexDirection: 'row', width: '100%', backgroundColor: 'pink', justifyContent: 'center'}}>
@@ -49,9 +91,17 @@ export default function ProfileScreen () {
             <Text style={{fontSize: 20, fontWeight: 'bold', padding: 20}}>My Connections</Text>
 
             <ScrollView style={{width: "100%", height: "100%"}}>
+                <TouchableOpacity style={{width: '100%', justifyContent: 'center', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, backgroundColor:"pink", borderRadius:10}} onPress={async () => {
+                    setModalVisible(true);
+                }}>
+                    <View>
+                        <Text style={{fontSize: 18, fontStyle:'bold', padding:20}}>Connect new app</Text>
+                    </View>
+                </TouchableOpacity>
+
                 <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
                 <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-                <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+
             </ScrollView>
         </View>
     )
