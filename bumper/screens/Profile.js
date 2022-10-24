8401eb22318b49a1477b7a5630f429981dca5419
islandandrock <islandandrock@gmail.com>
2022-10-24 10:00:35 -0700

    continue adding to app linking modal

--- bumper/screens/Profile.js
+++ bumper/screens/Profile.js
@@ -20,15 +20,18 @@ const x = async () => {
 }
 
 const SocialMedia = (props) => {
-    app = props.app;
-    if (!app) {
-        app = "instagram"
-    }
-    console.log(app,props.selectedApp)
+    let selectable=props.selectable?true:false
+    let app = props.app;
+    console.log("init", app,props.selectedApp)
     return (
-        <TouchableOpacity style={{backgroundcolor: (props.selectedApp==app)?"red":null, width: '100%', justifyContent: 'flex-start', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, paddingVertical: 10}} onclick={() => {
-            props.setSelectedApp(app)
-            console.log(props.selectedApp);
+        <TouchableOpacity style={{backgroundColor: props.selectedApp==app?"pink":null, width: '100%', justifyContent: 'flex-start', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, paddingVertical: 10}} onPress={() => {
+            if (selectable) {
+                console.log("clicked", app)
+                props.setSelectedApp(app)
+                console.log(app, props.selectedApp);
+            } else {
+                Linking.openURL(props.link)
+            }
         }}>
             <Image source={getIcon(app)} style={{height:48, width:48}} resizeMode="contain"/>
             <View>
@@ -38,8 +41,22 @@ const SocialMedia = (props) => {
     )
 }
 
+const StepOne = (props) => {
+    return (<View style={{width:"100%", flexGrow:1, flexShrink:1, backgroundColor:"mistyrose"}}>
+        <ScrollView style={{width:"100%"}}>
+        <SocialMedia selectable={true} name='Instagram' app="instagram" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+        <SocialMedia selectable={true} name='Facebook' app="facebook" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+        <SocialMedia selectable={true} name='Twitter' app="twitter" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+        <SocialMedia selectable={true} name='Youtube' app="youtube" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+        </ScrollView>
+    </View>)
+}
+
+
+
 const NewAppModal = (props) => {
     const [selectedApp, setSelectedApp] = useState(null);
+    const [step, setStep] = useState(1);
 
     return (
         <Modal
@@ -54,23 +71,21 @@ const NewAppModal = (props) => {
             <View style={{justifyContent:"center", alignItems:"center", height:"100%", width:"100%"}}>
             <View style={{width:"80%", height:"70%", backgroundColor:"#ffbab3", borderRadius:10, overflow:"hidden"}}>
                 <Text style={{textAlign:"center", fontWeight:"bold", fontSize:20, marginVertical:10}}>Link a New App</Text>
-                <TouchableOpacity style={{height:40, width:40, backgroundColor:"red", alignSelf:"flex-end", marginTop:-40}} onPress={() => props.setModalVisible(!props.modalVisible)}></TouchableOpacity>
-                <View style={{width:"100%", flexGrow:1, flexShrink:1, backgroundColor:"mistyrose"}}>
-                    <ScrollView style={{width:"100%"}}>
-                    <SocialMedia name='Instagram' app="instagram" selectedApp={selectedApp} setSelectedApp={setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-                    <SocialMedia name='Facebook' app="facebook" selectedApp={selectedApp} setSelectedApp={setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-                    <SocialMedia name='Twitter' app="twitter" selectedApp={selectedApp} setSelectedApp={setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-                    <SocialMedia name='Youtube' app="youtube" selectedApp={selectedApp} setSelectedApp={setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-                    </ScrollView>
-                
-                </View>
+                <TouchableOpacity style={{height:40, width:40, backgroundColor:"red", alignSelf:"flex-end", marginTop:-40}}
+                    onPress={() => props.setModalVisible(!props.modalVisible)}/>
+                <View style={{height:"100%", width:"100%", backgroundColor:"red"}}>
+                {
+                    {1: <StepOne style={{width:"100%", flexGrow:1, flexShrink:1, backgroundColor:"mistyrose"}} selectedApp={selectedApp} setSelectedApp={setSelectedApp}/>,
+                    2: <Text>hi</Text>}[step]
+                }
                 <View style={{width:"100%", backgroundColor:"#ffbab3", flexDirection:"row"}}>
-                    <TouchableOpacity style={{width:"50%", height:"100%"}}>
+                    <TouchableOpacity style={{width:"50%", height:"100%"}} onPress={() => props.setModalVisible(!props.modalVisible)}>
                         <Text style={{textAlign:"center", fontWeight:"bold", marginVertical:10}}>Cancel</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={{width:"50%", height:"100%", backgroundColor:"lightgrey"}} disabled={true}>
-                        <Text style={{textAlign:"center", fontWeight:"bold", marginVertical:10, color:"grey"}}>Choose App</Text>
+                    <TouchableOpacity style={{width:"50%", height:"100%", backgroundColor:selectedApp?null:"lightgrey"}} onPress={()=>{console.log("hi")}} disabled={selectedApp?false:true}>
+                        <Text style={{textAlign:"center", fontWeight:"bold", marginVertical:10, color:selectedApp?null:"grey"}}>Choose App</Text>
                     </TouchableOpacity>
+                </View>
                 </View>
             </View>
             </View>
@@ -93,7 +108,7 @@ export default function ProfileScreen () {
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             
-            <NewAppModal modalVisible={modalVisible} setModalVisible={setModalVisible}/>
+            {modalVisible? <NewAppModal modalVisible={modalVisible} setModalVisible={setModalVisible}/> : null}
 
             <View style={{justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'column', width: '100%', height: 220, backgroundColor: 'pink'}}>
                 <Text style={{fontSize: 40, fontWeight: 'bold', padding: 40}}>License Plate</Text>
@@ -107,13 +122,11 @@ export default function ProfileScreen () {
                 <TouchableOpacity style={{width: '100%', justifyContent: 'center', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, backgroundColor:"pink", borderRadius:10}} onPress={async () => {
                     setModalVisible(true);
                 }}>
-                    <View>
-                        <Text style={{fontSize: 18, fontStyle:'bold', padding:20}}>Connect new app</Text>
-                    </View>
+                    <Text style={{fontSize: 18, fontStyle:'bold', padding:20}}>Connect new app</Text>
                 </TouchableOpacity>
 
-                <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-                <SocialMedia name='@Placeholder' link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+                <SocialMedia name='@Placeholder' app="instagram" link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+                <SocialMedia name='@Placeholder' app="youtube" link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
 
             </ScrollView>
         </View>
