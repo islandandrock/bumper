ccc1be33b421e1022ecae4dfb77c93e9906df6e1
islandandrock <islandandrock@gmail.com>
2022-11-08 08:40:17 -0800

    fix indentation to 2 spaces in js

--- bumper/screens/Friends.js
+++ bumper/screens/Friends.js
@@ -6,110 +6,110 @@ import { getData } from '../util/storage'
 
 
 const SearchBar = (props) => {
-    return (
-        <View>
-            <TextInput style={styles.input} placeholder='Search' value={props.SearchText} onChangeText={(text)=>props.SetSearchText(text)}/>
-        </View>
+  return (
+    <View>
+      <TextInput style={styles.input} placeholder='Search' value={props.SearchText} onChangeText={(text)=>props.SetSearchText(text)}/>
+    </View>
 
-    )
+  )
 
 }
 
 
 
 export default function FriendScreen ( {navigation} ) {
-    const [SearchText, SetSearchText] = useState('');
-    const [ListMode, SetListMode] = useState(true)
-    const [user_id, setUser_id] = useState("")
-    const [friends, setFriends] = useState([])
-    const [refresh, forceRefresh] = useState(false)
+  const [SearchText, SetSearchText] = useState('');
+  const [ListMode, SetListMode] = useState(true)
+  const [user_id, setUser_id] = useState("")
+  const [friends, setFriends] = useState([])
+  const [refresh, forceRefresh] = useState(false)
 
-    useEffect(() => {
-        const asyncFunc = async () => {
-          let user_id_temp = await getData("user_id");
-          setUser_id(user_id_temp);
-          let temp = await getFriend(user_id_temp)
-          setFriends(temp)
-        }
-        asyncFunc();
-    }, [refresh])
+  useEffect(() => {
+    const asyncFunc = async () => {
+      let user_id_temp = await getData("user_id");
+      setUser_id(user_id_temp);
+      let temp = await getFriend(user_id_temp)
+      setFriends(temp)
+    }
+    asyncFunc();
+  }, [refresh])
 
-    const Pins = friends.map((friend) => <Marker onPress={() => Linking.openURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')} key={friends.indexOf(friend)} coordinate={{latitude: 37.78825, longitude: -122.4324}} pinColor={'pink'}>
-    <Text style={{backgroundColor: 'pink', borderRadius: 100, padding: 5}}>{friend.username}</Text></Marker>)
+  const Pins = friends.map((friend) => <Marker onPress={() => Linking.openURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')} key={friends.indexOf(friend)} coordinate={{latitude: 37.78825, longitude: -122.4324}} pinColor={'pink'}>
+  <Text style={{backgroundColor: 'pink', borderRadius: 100, padding: 5}}>{friend.username}</Text></Marker>)
 
-    return (
-        <View>
-            <View style={styles.container}>
-                <SearchBar SearchText={SearchText} SetSearchText={SetSearchText}/>
-            </View>
-            <View style={{flexDirection: 'row', justifyContent: 'flex-end', margin: 10}}>
-                <TouchableOpacity style={styles.toggle} onPress={() => SetListMode(!ListMode)}>
-                    <Text style={{fontSize: 18, fontWeight: 'bold'}}>Toggle Mode</Text>
-                </TouchableOpacity>
-            </View>
-            {ListMode ? 
-            (
-                
-                <View style={{flexDirection: 'column', justifyContent: 'flex-start'}}>
-                    <Button style={{height:50, width:50, backgroundColor:"red"}} title="RELOAD" onPress={() => forceRefresh(!refresh)}></Button>
-                    <FlatList data={friends} 
-                    renderItem={({item}) => <TouchableOpacity onPress={() => Linking.openURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}><Text style={styles.friend}>{item.username}</Text></TouchableOpacity>}/>
-                </View>
-            ):(
-                <View style={{justifyContent: 'center', flexDirection: 'column'}}>
-                    <MapView     initialRegion={{
-                                    latitude: 37.78825,
-                                    longitude: -122.4324,
-                                    latitudeDelta: 0.0922,
-                                    longitudeDelta: 0.0421,
-                                    }} style={styles.map}>
-                                {Pins}
-                    </MapView>
-                </View>
-            )}
+  return (
+    <View>
+      <View style={styles.container}>
+        <SearchBar SearchText={SearchText} SetSearchText={SetSearchText}/>
+      </View>
+      <View style={{flexDirection: 'row', justifyContent: 'flex-end', margin: 10}}>
+        <TouchableOpacity style={styles.toggle} onPress={() => SetListMode(!ListMode)}>
+          <Text style={{fontSize: 18, fontWeight: 'bold'}}>Toggle Mode</Text>
+        </TouchableOpacity>
+      </View>
+      {ListMode ? 
+      (
+        
+        <View style={{flexDirection: 'column', justifyContent: 'flex-start'}}>
+          <Button style={{height:50, width:50, backgroundColor:"red"}} title="RELOAD" onPress={() => forceRefresh(!refresh)}></Button>
+          <FlatList data={friends} 
+          renderItem={({item}) => <TouchableOpacity onPress={() => Linking.openURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}><Text style={styles.friend}>{item.username}</Text></TouchableOpacity>}/>
         </View>
-    )
+      ):(
+        <View style={{justifyContent: 'center', flexDirection: 'column'}}>
+          <MapView   initialRegion={{
+                  latitude: 37.78825,
+                  longitude: -122.4324,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                  }} style={styles.map}>
+                {Pins}
+          </MapView>
+        </View>
+      )}
+    </View>
+  )
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        margin: 10
-    },
+  container: {
+    margin: 10
+  },
 
-    marker: {
-        backgroundColor: 'pink',
-    },
+  marker: {
+    backgroundColor: 'pink',
+  },
 
-    toggle: {
-        backgroundColor:'pink',
-        padding: 10,
-        borderRadius: 10
-    },
+  toggle: {
+    backgroundColor:'pink',
+    padding: 10,
+    borderRadius: 10
+  },
 
-    input: {
-        borderRadius:10,
-        padding:10,
-        backgroundColor: '#fff',
-        borderWidth:1,
-        borderColor: 'pink'
-    },
+  input: {
+    borderRadius:10,
+    padding:10,
+    backgroundColor: '#fff',
+    borderWidth:1,
+    borderColor: 'pink'
+  },
 
-    map: {
-        width: '100%',
-        height: '100%',
-        borderRadius:10,
-        marginRight:10,
-        justifyContent:'center'
-    },
+  map: {
+    width: '100%',
+    height: '100%',
+    borderRadius:10,
+    marginRight:10,
+    justifyContent:'center'
+  },
 
-    friend: {
-        width: '100%',
-        fontSize: 20,
-        fontWeight: 'bold',
-        padding:10,
-        paddingLeft:20,
-        backgroundColor: '#FFDADA',
-        borderBottomColor: 'black',
-    }
+  friend: {
+    width: '100%',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding:10,
+    paddingLeft:20,
+    backgroundColor: '#FFDADA',
+    borderBottomColor: 'black',
+  }
 })
--- bumper/screens/Profile.js
+++ bumper/screens/Profile.js
@@ -7,187 +7,187 @@ import { addConnection, addFriend, getConnections, getUser } from '../util/reque
 import getIcon from '../util/icons';
 
 const TextBar = (props) => {
-    return (
-      <View style={{width:"100%", alignItems:"center", marginBottom:20, marginTop:5}}>
-        <TextInput style={styles.input} placeholder={props.placeholder} value={props.inputText} onChangeText={(text)=>props.setInputText(text)}/>
-      </View>
-    )
+  return (
+    <View style={{width:"100%", alignItems:"center", marginBottom:20, marginTop:5}}>
+    <TextInput style={styles.input} placeholder={props.placeholder} value={props.inputText} onChangeText={(text)=>props.setInputText(text)}/>
+    </View>
+  )
   }
 
 const SocialMedia = (props) => {
-    let selectable=props.selectable?true:false
-    let app = props.app;
-    console.log("init", app,props.selectedApp)
-    return (
-        <TouchableOpacity style={{backgroundColor: props.selectedApp==app?"pink":null, width: '100%', justifyContent: 'flex-start', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, paddingVertical: 10}} onPress={() => {
-            if (selectable) {
-                console.log("clicked", app)
-                props.setSelectedApp(app)
-                console.log(app, props.selectedApp);
-            } else {
-                Linking.openURL(props.link)
-            }
-        }}>
-            <Image source={getIcon(app)} style={{height:48, width:48}} resizeMode="contain"/>
-            <View>
-                <Text style={{fontSize: 18, fontStyle:'bold', padding:20}}>{props.name}</Text>
-            </View>
-        </TouchableOpacity>
-    )
+  let selectable=props.selectable?true:false
+  let app = props.app;
+  console.log("init", app,props.selectedApp)
+  return (
+    <TouchableOpacity style={{backgroundColor: props.selectedApp==app?"pink":null, width: '100%', justifyContent: 'flex-start', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, paddingVertical: 10}} onPress={() => {
+      if (selectable) {
+        console.log("clicked", app)
+        props.setSelectedApp(app)
+        console.log(app, props.selectedApp);
+      } else {
+        Linking.openURL(props.link)
+      }
+    }}>
+      <Image source={getIcon(app)} style={{height:48, width:48}} resizeMode="contain"/>
+      <View>
+        <Text style={{fontSize: 18, fontStyle:'bold', padding:20}}>{props.name}</Text>
+      </View>
+    </TouchableOpacity>
+  )
 }
 
 const StepOne = (props) => {
-    return (<View style={{width:"100%", flexGrow:1, flexShrink:1, backgroundColor:"mistyrose"}}>
-        <ScrollView style={{width:"100%"}}>
-            <SocialMedia selectable={true} name='Instagram' app="instagram" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-            <SocialMedia selectable={true} name='Facebook' app="facebook" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-            <SocialMedia selectable={true} name='Twitter' app="twitter" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-            <SocialMedia selectable={true} name='Youtube' app="youtube" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
-        </ScrollView>
-    </View>)
+  return (<View style={{width:"100%", flexGrow:1, flexShrink:1, backgroundColor:"mistyrose"}}>
+    <ScrollView style={{width:"100%"}}>
+      <SocialMedia selectable={true} name='Instagram' app="instagram" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+      <SocialMedia selectable={true} name='Facebook' app="facebook" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+      <SocialMedia selectable={true} name='Twitter' app="twitter" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+      <SocialMedia selectable={true} name='Youtube' app="youtube" selectedApp={props.selectedApp} setSelectedApp={props.setSelectedApp} link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'/>
+    </ScrollView>
+  </View>)
 }
 
 const StepTwo = (props) => {
-    return (<View style={{width:"100%", flexGrow:1, flexShrink:1, backgroundColor:"mistyrose"}}>
-        <Text style={{textAlign:"center", marginVertical:10}}>Enter your {props.selectedApp.charAt(0).toUpperCase() + props.selectedApp.slice(1)} username:</Text>
-        <TextBar inputText={props.username} setInputText={props.setUsername} placeholder={"Your username"}/>
-    </View>)
+  return (<View style={{width:"100%", flexGrow:1, flexShrink:1, backgroundColor:"mistyrose"}}>
+    <Text style={{textAlign:"center", marginVertical:10}}>Enter your {props.selectedApp.charAt(0).toUpperCase() + props.selectedApp.slice(1)} username:</Text>
+    <TextBar inputText={props.username} setInputText={props.setUsername} placeholder={"Your username"}/>
+  </View>)
 }
 
 function usernameToLink(app_name, username) {
-    let base_links = {instagram:"instagram.com", facebook:"facebook.com", twitter:"twitter.com", youtube:"youtube.com"};
-    return "https://www." + app_name + ".com/" + username
+  let base_links = {instagram:"instagram.com", facebook:"facebook.com", twitter:"twitter.com", youtube:"youtube.com"};
+  return "https://www." + app_name + ".com/" + username
 }
 
 const connectApp = async (app_name, link) => {
-    try {
-        await addConnection(app_name, link);
-        Alert.alert("App connected!");
-    } catch (e) {
-        if (isCode(e, [422])) {
-        Alert.alert("Connection failed!", "Link a valid account.")
-        } else {
-        throw(e);
-        }
-    } 
+  try {
+    await addConnection(app_name, link);
+    Alert.alert("App connected!");
+  } catch (e) {
+    if (isCode(e, [422])) {
+    Alert.alert("Connection failed!", "Link a valid account.")
+    } else {
+    throw(e);
+    }
+  } 
 }
 
 const NewAppModal = (props) => {
-    const [step, setStep] = useState(1);
-    const [nextText, setNextText] = useState("Choose App")
+  const [step, setStep] = useState(1);
+  const [nextText, setNextText] = useState("Choose App")
 
-    const [selectedApp, setSelectedApp] = useState(null);
-    const [username, setUsername] = useState("")
+  const [selectedApp, setSelectedApp] = useState(null);
+  const [username, setUsername] = useState("")
 
-    return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={props.modalVisible}
-            onRequestClose={() => {
-                props.setModalVisible(!props.modalVisible);
-            }}
-            style={{alignContent:"center", alignItems:"center", justifyContent:"center"}}
-        >
-            <View style={{justifyContent:"center", alignItems:"center", height:"100%", width:"100%"}}>
-            <View style={{width:"80%", height:"70%", backgroundColor:"#ffbab3", borderRadius:10, overflow:"hidden"}}>
-                <Text style={{textAlign:"center", fontWeight:"bold", fontSize:20, marginVertical:10}}>Link a New App</Text>
-                <TouchableOpacity style={{height:40, width:40, backgroundColor:"red", alignSelf:"flex-end", marginTop:-40}}
-                    onPress={() => props.setModalVisible(!props.modalVisible)}/>
-                {
-                    {1: <StepOne style={{width:"100%"}} selectedApp={selectedApp} setSelectedApp={setSelectedApp}/>,
-                    2: <StepTwo style={{width:"100%"}} username={username} setUsername={setUsername} selectedApp={selectedApp}/>}[step]
-                }
-                <View style={{width:"100%", backgroundColor:"#ffbab3", flexDirection:"row"}}>
-                    <TouchableOpacity style={{width:"50%", height:"100%"}} onPress={() => props.setModalVisible(!props.modalVisible)}>
-                        <Text style={{textAlign:"center", fontWeight:"bold", marginVertical:10}}>Cancel</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{width:"50%", height:"100%", backgroundColor:selectedApp?null:"lightgrey"}} onPress={async ()=>{setNextText("Link App");
-                        if (step < 2) {setStep(step+1)} else {connectApp(selectedApp, usernameToLink(selectedApp, username)); props.setModalVisible(!props.modalVisible)}; props.forceReload(!props.reload)}} disabled={selectedApp?false:true}>
-                        <Text style={{textAlign:"center", fontWeight:"bold", marginVertical:10, color:selectedApp?null:"grey"}}>{nextText}</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            </View>
-        </Modal>
-    )
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={props.modalVisible}
+      onRequestClose={() => {
+        props.setModalVisible(!props.modalVisible);
+      }}
+      style={{alignContent:"center", alignItems:"center", justifyContent:"center"}}
+    >
+      <View style={{justifyContent:"center", alignItems:"center", height:"100%", width:"100%"}}>
+      <View style={{width:"80%", height:"70%", backgroundColor:"#ffbab3", borderRadius:10, overflow:"hidden"}}>
+        <Text style={{textAlign:"center", fontWeight:"bold", fontSize:20, marginVertical:10}}>Link a New App</Text>
+        <TouchableOpacity style={{height:40, width:40, backgroundColor:"red", alignSelf:"flex-end", marginTop:-40}}
+          onPress={() => props.setModalVisible(!props.modalVisible)}/>
+        {
+          {1: <StepOne style={{width:"100%"}} selectedApp={selectedApp} setSelectedApp={setSelectedApp}/>,
+          2: <StepTwo style={{width:"100%"}} username={username} setUsername={setUsername} selectedApp={selectedApp}/>}[step]
+        }
+        <View style={{width:"100%", backgroundColor:"#ffbab3", flexDirection:"row"}}>
+          <TouchableOpacity style={{width:"50%", height:"100%"}} onPress={() => props.setModalVisible(!props.modalVisible)}>
+            <Text style={{textAlign:"center", fontWeight:"bold", marginVertical:10}}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{width:"50%", height:"100%", backgroundColor:selectedApp?null:"lightgrey"}} onPress={async ()=>{setNextText("Link App");
+            if (step < 2) {setStep(step+1)} else {connectApp(selectedApp, usernameToLink(selectedApp, username)); props.setModalVisible(!props.modalVisible)}; props.forceReload(!props.reload)}} disabled={selectedApp?false:true}>
+            <Text style={{textAlign:"center", fontWeight:"bold", marginVertical:10, color:selectedApp?null:"grey"}}>{nextText}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      </View>
+    </Modal>
+  )
 }
 
 
 export default function ProfileScreen ( {navigation, route} ) {
-    const [username, setUsername] = useState("")
-    const [userId, setUserId] = useState(null)
-    const [modalVisible, setModalVisible] = useState(false);
-    const [connectedApps, setConnectedApps] = useState([]);
-    const [isOwnProfile, setIsOwnProfile] = useState(false);
-    const [reload, forceReload] = useState(false)
+  const [username, setUsername] = useState("")
+  const [userId, setUserId] = useState(null)
+  const [modalVisible, setModalVisible] = useState(false);
+  const [connectedApps, setConnectedApps] = useState([]);
+  const [isOwnProfile, setIsOwnProfile] = useState(false);
+  const [reload, forceReload] = useState(false)
 
-    useEffect(() => {
-        const asyncFunc = async () => {
-            let signedInId = await getData("user_id")
-            let signedInUser = await getData("username")
-            if (!route.params?.id) {
-                if (!route.params) {route.params = {}}
-                route.params.id = signedInId;
-            }
-            if (route.params.id == signedInId) {
-                setIsOwnProfile(true);
-                route.params.username = signedInUser;
-            } else {
-                let user = await getUser(route.params.id);
-                route.params.username = user.username;
-                navigation.setOptions({title:`${route.params.username}'s Profile`})
-                navigation.setOptions({
-                    headerRight: () => (
-                    <Button onPress={() => addFriend(route.params.id)} title="Add friend" />
-                    ),
-                });
-            }
-            setUsername(route.params.username);
-            setUserId(route.params.id)
-            let temp = await getConnections(route.params.id)
-            setConnectedApps(temp)
-        }
+  useEffect(() => {
+    const asyncFunc = async () => {
+      let signedInId = await getData("user_id")
+      let signedInUser = await getData("username")
+      if (!route.params?.id) {
+        if (!route.params) {route.params = {}}
+        route.params.id = signedInId;
+      }
+      if (route.params.id == signedInId) {
+        setIsOwnProfile(true);
+        route.params.username = signedInUser;
+      } else {
+        let user = await getUser(route.params.id);
+        route.params.username = user.username;
+        navigation.setOptions({title:`${route.params.username}'s Profile`})
+        navigation.setOptions({
+          headerRight: () => (
+          <Button onPress={() => addFriend(route.params.id)} title="Add friend" />
+          ),
+        });
+      }
+      setUsername(route.params.username);
+      setUserId(route.params.id)
+      let temp = await getConnections(route.params.id)
+      setConnectedApps(temp)
+    }
 
-        asyncFunc();
-    }, [route.params, reload])
-    
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            
-            {modalVisible? <NewAppModal modalVisible={modalVisible} setModalVisible={setModalVisible} forceReload={forceReload} reload={reload}/> : null}
+    asyncFunc();
+  }, [route.params, reload])
+  
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      
+      {modalVisible? <NewAppModal modalVisible={modalVisible} setModalVisible={setModalVisible} forceReload={forceReload} reload={reload}/> : null}
 
-            <View style={{justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'column', width: '100%', height: 220, backgroundColor: 'pink'}}>
-                <Text style={{fontSize: 40, fontWeight: 'bold', padding: 40}}>License Plate</Text>
-                <View style={{flexDirection: 'row', width: '100%', backgroundColor: 'pink', justifyContent: 'center'}}>
-                    <Text style={{fontSize: 20, fontWeight: 'bold', padding: 20}}>{username}</Text>
-                </View>
-            </View>
-            <Text style={{fontSize: 20, fontWeight: 'bold', padding: 20}}>{isOwnProfile? "My" : `${username}'s`} Connections</Text>
-
-            <ScrollView style={{width: "100%", height: "100%"}}>
-                {isOwnProfile ?  <TouchableOpacity style={{width: '100%', justifyContent: 'center', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, backgroundColor:"pink", borderRadius:10}} onPress={async () => {
-                    setModalVisible(true);
-                }}>
-                    <Text style={{fontSize: 18, fontStyle:'bold', padding:20}}>Connect new app</Text>
-                </TouchableOpacity> : null}
-
-                {connectedApps.map((connection) => <SocialMedia name={connection.app_name} app={connection.app_name} link={connection.link} key={connection.id}/>)}
-
-            </ScrollView>
+      <View style={{justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'column', width: '100%', height: 220, backgroundColor: 'pink'}}>
+        <Text style={{fontSize: 40, fontWeight: 'bold', padding: 40}}>License Plate</Text>
+        <View style={{flexDirection: 'row', width: '100%', backgroundColor: 'pink', justifyContent: 'center'}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold', padding: 20}}>{username}</Text>
         </View>
-    )
+      </View>
+      <Text style={{fontSize: 20, fontWeight: 'bold', padding: 20}}>{isOwnProfile? "My" : `${username}'s`} Connections</Text>
+
+      <ScrollView style={{width: "100%", height: "100%"}}>
+        {isOwnProfile ?  <TouchableOpacity style={{width: '100%', justifyContent: 'center', alignItems:'center', flexDirection: 'row', paddingHorizontal: 10, backgroundColor:"pink", borderRadius:10}} onPress={async () => {
+          setModalVisible(true);
+        }}>
+          <Text style={{fontSize: 18, fontStyle:'bold', padding:20}}>Connect new app</Text>
+        </TouchableOpacity> : null}
+
+        {connectedApps.map((connection) => <SocialMedia name={connection.app_name} app={connection.app_name} link={connection.link} key={connection.id}/>)}
+
+      </ScrollView>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    input: {
-      borderRadius:10,
-      padding:10,
-      backgroundColor: '#fff',
-      borderWidth:1,
-      borderColor: 'pink',
-      width:"80%"
-    },
+  input: {
+    borderRadius:10,
+    padding:10,
+    backgroundColor: '#fff',
+    borderWidth:1,
+    borderColor: 'pink',
+    width:"80%"
+  },
   
-    
+  
 })
