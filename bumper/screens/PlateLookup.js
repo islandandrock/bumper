e53692121725d3ef007a7b91b049bfdcbc9f03c4
aaaaahhh <blewconklin0264@student.pps.net>
2023-02-01 09:59:08 -0800

    using system camera

--- bumper/screens/PlateLookup.js
+++ bumper/screens/PlateLookup.js
@@ -1,9 +1,9 @@
 import { useLinkProps } from '@react-navigation/native';
 import { Camera, CameraType } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 import {View, Text, TouchableOpacity, StyleSheet, TextInput, FlatList, Linking} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useState, useEffect, useRef } from 'react';
-import * as MediaLibrary from 'expo-media-library';
 
 
 import {signUp, userSearch} from "../util/requests"
@@ -26,85 +26,54 @@ const SearchBar = (props) => {
 }
 
 export default function PlateLookupScreen ({ navigation }) {
-  let cameraRef = useRef();
   const [SearchText, SetSearchText] = useState('');
   const [SearchUsers, SetSearchUsers] = useState([])
-  const [type, setType] = useState(CameraType.back);
-  const [permission, setPermission] = useState(null)
-  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
-  const [cameraMode, setCameraMode] = useState(false)
-
 
   useEffect(() => {
     const asyncFunc = async () => {
       SetSearchUsers(await userSearch(SearchText))
-      const permissionStatus = await Camera.requestCameraPermissionsAsync();
-      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
-      setPermission(permissionStatus.status === 'granted');
-      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
     }
     asyncFunc();
   }, [])
 
-  let takePic = async () => {
-    let options = {
-      quality: 1,
-      base64: true,
-      exif: false
-    };
+  const takePicture = async () => {
+    let result = await ImagePicker.launchCameraAsync()
+  }
 
-    let newPhoto = await cameraRef.current.takePictureAsync(options);
-
-    MediaLibrary.saveToLibraryAsync(newPhoto.uri)
-  };
-
-  return cameraMode ? 
-    (
-      <View style={{width:"100%", height:"100%"}}>
-        <Camera type={type} style={{flex:1}} ref={cameraRef}></Camera>
-        <View style={{flexDirection:"row"}}>
-          <PlateButton text={"TAKE PICTURE"} onPress={takePic}/>
-        </View>
-        <View style={{flexDirection: 'row', justifyContent: 'flex-end', margin: 10}}>
-          <TouchableOpacity style={styles.toggle} onPress={() => setCameraMode(!cameraMode)}>
-            <Text style={{fontSize: 18, fontWeight: 'bold'}}>Toggle Mode</Text>
-          </TouchableOpacity>
-        </View>
+  return (
+    <View style={{flexDirection: 'column', justifyContent: 'flex-start', flex:1, flexGrow:1}}>
+      <View style={styles.container}>
+        <SearchBar SearchText={SearchText} SetSearchText={SetSearchText}/>       
+        <TouchableOpacity style={{width:'30%', backgroundColor:"pink", borderRadius:10, justifyContent:'center', marginLeft:5}} onPress={async ()=>SetSearchUsers(await userSearch(SearchText))}>
+          <Text style={{fontWeight:"bold", fontSize:20, textAlign:"center"}}>Search</Text>
+        </TouchableOpacity>
       </View>
-    ) : (
+      <View style={{flexDirection:'row', justifyContent:'flex-end', height:60, padding:10}}>
+        <TouchableOpacity style={{width:'40%', height:'100%', backgroundColor:"pink", borderRadius:10, justifyContent:'center', marginLeft:5}} onPress={async () => await takePicture() }>
+          <Text style={{fontWeight:"bold", fontSize:20, textAlign:"center"}}>Take Picture</Text>
+        </TouchableOpacity>
+      </View>
       <View style={{flexDirection: 'column', justifyContent: 'flex-start', flex:1, flexGrow:1}}>
-        <View style={styles.container}>
-          <SearchBar SearchText={SearchText} SetSearchText={SetSearchText}/>       
-          <TouchableOpacity style={{width:'30%', backgroundColor:"pink", borderRadius:10, justifyContent:'center', marginLeft:5}} onPress={async ()=>SetSearchUsers(await userSearch(SearchText))}>
-            <Text style={{fontWeight:"bold", fontSize:20, textAlign:"center"}}>Search</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{flexDirection: 'row', justifyContent: 'flex-end', margin: 10}}>
-          <TouchableOpacity style={styles.toggle} onPress={() => setCameraMode(!cameraMode)}>
-            <Text style={{fontSize: 18, fontWeight: 'bold'}}>Toggle Mode</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{flexDirection: 'column', justifyContent: 'flex-start', flex:1, flexGrow:1}}>
-          <ScrollView style={{width: "100%"}}>
-            {SearchUsers.map((user) => 
-              <TouchableOpacity style={[styles.userList]} key={user.id} onPress={() => navigation.navigate("Profile", {id:user.id})}>
-                <LicensePlate width={80} plate={user.plate} state={user.linked ? "oregon" : "unlinked"} style={{marginRight:20}}/>
-                <View style={{flexGrow:1, flexShrink:1}}>
-                  <Text style={[styles.user]} numberOfLines={1}>{user.name}</Text>
-                </View>
-                <View style={{width:60, justifyContent:"center", alignItems:"center"}}>
-                  <Text style={{fontWeight:"bold", fontSize:20}}>{user.numFriends}</Text>
-                  <Text style={{marginTop:-5, fontSize:10}}>Friends</Text>
-                </View>
-                <View style={{width:60, justifyContent:"center", alignItems:"center"}}>
-                  <Text style={{fontWeight:"bold", fontSize:20}}>{user.numConnections}</Text>
-                  <Text style={{marginTop:-5, fontSize:10}}>Connections</Text>
-                </View>
-              </TouchableOpacity>
-            )}
-          </ScrollView>
-        </View>
+        <ScrollView style={{width: "100%"}}>
+          {SearchUsers.map((user) => 
+            <TouchableOpacity style={[styles.userList]} key={user.id} onPress={() => navigation.navigate("Profile", {id:user.id})}>
+              <LicensePlate width={80} plate={user.plate} state={user.linked ? "oregon" : "unlinked"} style={{marginRight:20}}/>
+              <View style={{flexGrow:1, flexShrink:1}}>
+                <Text style={[styles.user]} numberOfLines={1}>{user.name}</Text>
+              </View>
+              <View style={{width:60, justifyContent:"center", alignItems:"center"}}>
+                <Text style={{fontWeight:"bold", fontSize:20}}>{user.numFriends}</Text>
+                <Text style={{marginTop:-5, fontSize:10}}>Friends</Text>
+              </View>
+              <View style={{width:60, justifyContent:"center", alignItems:"center"}}>
+                <Text style={{fontWeight:"bold", fontSize:20}}>{user.numConnections}</Text>
+                <Text style={{marginTop:-5, fontSize:10}}>Connections</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        </ScrollView>
       </View>
+    </View>
   )
 }
 
