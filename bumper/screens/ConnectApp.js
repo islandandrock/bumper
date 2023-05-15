07fc1fcd5e1480cf712738bac5a3d87a4d55a668
islandandrock <islandandrock@gmail.com>
2023-05-15 08:36:10 -0700

    Initial commit of Connect App Screen

---
+++ bumper/screens/ConnectApp.js
@@ -0,0 +1,133 @@
+import {useState, useEffect} from 'react'
+import { DropdownSearch } from '../util/components'
+import { View, Text, TextInput, Alert, StyleSheet, TouchableOpacity, Keyboard } from 'react-native';
+import { signIn, updateUser, isCode } from '../util/requests';
+import { storeData, getData } from '../util/storage';
+
+const TextBar = (props) => {
+  return (
+    <View style={{width:"100%", alignItems:"center", marginBottom:20, marginTop:5}}>
+      <TextInput style={styles.input} placeholder={props.placeholder} value={props.inputText} onChangeText={(text)=>props.setInputText(text)}/>
+    </View>
+  )
+}
+
+function BigButton (props) {
+  return (
+    <TouchableOpacity style={{width:"50%", height:100, backgroundColor:"pink", justifyContent:"center", borderRadius:20}} onPress={props.onPress}>
+      <Text style={{fontWeight:"bold", fontSize:20, textAlign:"center"}}>{props.text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default function EditProfileScreen ({ navigation, route }) {
+  const [name, setName] = useState(route.params.name)
+  const [bio, setBio] = useState(route.params.bio)
+  const [plate, setPlate] = useState(route.params.plate.linked ? route.params.plate.plate : '')
+  const [plateState, setPlateState] = useState(route.params.plateState)
+  const [selected, setSelected] = useState(undefined);
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+  const [isChangingPlate, setIsChangingPlate] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardVisible(true); // or some other action
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardVisible(false); // or some other action
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, [])
+
+    
+  const data = [
+    { label: 'Classic Oregon', value: '1' },
+    { label: 'Crater Lake', value: '3' },
+    { label: 'Blue & Yellow', value: '2' },
+  ];
+  
+  const renderItem = item => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.textItem}>{item.label}</Text>
+        {item.value === value && (
+          <AntDesign
+            style={styles.icon}
+            color="black"
+            name="Safety"
+            size={20}
+          />
+        )}
+      </View>
+    );
+  };
+
+  return (
+    <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+      <Text style={{fontWeight: 'bold', fontSize: 20}}>Name</Text>
+      <TextBar inputText={name} setInputText={setName} placeholder="Your name"/>
+      <Text style={{fontWeight: 'bold', fontSize: 20}}>Bio</Text>
+      <TextBar inputText={bio} setInputText={setBio} placeholder="Your bio"/>
+      <Text style={{fontWeight: 'bold', fontSize: 20}}>Plate</Text>
+      <TextBar inputText={plate} setInputText={setPlate} placeholder="Your license plate"/>
+      <View style={{ width:'100%', alignItems:'center'}}>
+        {/*
+        <TouchableOpacity style={styles.changePlateState} onPress={() => setPlateState('oregon')}><Text style={{fontSize: 20}}>Oregon</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.changePlateState} onPress={() => setPlateState('california')}><Text style={{fontSize: 20}}>California</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.changePlateState} onPress={() => setPlateState('unlinked')}><Text style={{fontSize: 20}}>Unlinked</Text></TouchableOpacity>
+        */}
+        <DropdownSearch placeholder="Plate Style" data={data} setPlateState={setPlateState} dropdownPos={isKeyboardVisible? 'top' : 'bottom'}/>
+      </View>
+      
+      <BigButton text="SAVE" onPress={
+        async () => {
+          try { 
+            await updateUser(name, bio, plate, plateState);
+            Alert.alert("Updated your profile!");
+            navigation.pop();
+          } catch (e) {
+            if (isCode(e, [409])) {
+              Alert.alert("Plate update failed!", "Someone has already linked this license plate.")
+            } else if (isCode(e, [422])) {
+              Alert.alert("Profile update failed!", e.description)
+            } else {
+              throw(e);
+            }
+          }
+        }
+      }/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius:10,
+    padding:10,
+    backgroundColor: '#fff',
+    borderWidth:1,
+    borderColor: 'pink',
+    width:"80%"
+  },
+  changePlateState: {
+    padding: 10,
+    backgroundColor: 'pink',
+    borderRadius: 10,
+    margin: 10
+  },
+  dropDownStyle: {
+    backgroundColor: 'red',
+    height:50,
+    width: 100
+  }
+})
